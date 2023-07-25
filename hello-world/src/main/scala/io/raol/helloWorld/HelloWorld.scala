6fda0525c3ae488b47532d80382c97d3c3fba5be
Oleg Rakitskiy <oleg.rakitskiy@betssongroup.com>
2023-07-25 23:56:22 +0200

    Add hello world

---
+++ hello-world/src/main/scala/io/raol/helloWorld/HelloWorld.scala
@@ -0,0 +1,28 @@
+package io.raol.helloWorld
+
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    val logger = Logger.getRootLogger;
+
+    val conf = new SparkConf()
+      .setAppName(HelloWorld.getClass.getName)
+
+    val spark = SparkSession.builder
+      .config(conf)
+      .getOrCreate()
+
+    var df = spark.read
+      .format("csv")
+      .option("header", true)
+      .option("inferSchema", true)
+      .load("/data/books.csv")
+
+    logger.info("Loaded dataset")
+
+    logger.info(s"Record count ${df.count()}")
+  }
+}
